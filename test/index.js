e944c1c98fa8ff1df30261d7a5ed386370c8d133
Thorsten Lorenz <thlorenz@gmx.de>
2013-12-13 16:54:53 -0500

    handling multi sel/rule and multi rules

--- test/index.js
+++ test/index.js
@@ -27,11 +27,119 @@ test('\nsimple css with one rule', function (t) {
     .on('data', function (d) { data += d })
     .on('end', function () {
       var lines = data.split('\n');
-//      inspect(lines);
+      inspect(lines);
       t.deepEqual(
           lines
         , [ '.jsdoc a {',
             '  color: \'green\';',
+            '}' ]
+      )
+      t.end()
+    })
+})
+
+test('\nmultiple selectors per rule, two rules', function (t) {
+  var data = '';
+
+  fs.createReadStream(path.join(fixtures, 'multi-selectors-per-rule.css'), 'utf8')
+    .on('error', fail.bind(t))
+    .pipe(namespace({ selector: '.jsdoc' }))
+    .on('error', fail.bind(t))
+    .on('data', function (d) { data += d })
+    .on('end', function () {
+      var lines = data.split('\n');
+      inspect(lines);
+      t.deepEqual(
+          lines
+        , [ '.jsdoc nav h2 a,',
+            '.jsdoc nav h2 a:visited {',
+            '  text-decoration: none;',
+            '}',
+            '',
+            '.jsdoc nav h2 a,',
+            '.jsdoc nav h2 a:visited,',
+            '.jsdoc div.hello {',
+            '  text-decoration: none;',
+            '}' ]
+      )
+      t.end()
+    })
+})
+
+test('\nmultiple rules', function (t) {
+  var data = '';
+
+  fs.createReadStream(path.join(fixtures, 'multi-rules.css'), 'utf8')
+    .on('error', fail.bind(t))
+    .pipe(namespace({ selector: '.jsdoc' }))
+    .on('error', fail.bind(t))
+    .on('data', function (d) { data += d })
+    .on('end', function () {
+      var lines = data.split('\n');
+      inspect(lines);
+      t.deepEqual(
+          lines
+        , [ '.jsdoc html {',
+            '  overflow: auto;',
+            '}',
+            '',
+            '.jsdoc body {',
+            '  font: 14px "DejaVu Sans Condensed", "Liberation Sans", "Nimbus Sans L", Tahoma, Geneva, "Helvetica Neue", Helvetica, Arial, sans-serif;',
+            '}',
+            '',
+            '.jsdoc a {',
+            '  color: #444;',
+            '}',
+            '',
+            '.jsdoc a:visited {',
+            '  color: #444;',
+            '}',
+            '',
+            '.jsdoc a:active {',
+            '  color: #444;',
+            '}',
+            '',
+            '.jsdoc header {',
+            '  display: block;',
+            '  padding: 6px 4px;',
+            '}',
+            '',
+            '.jsdoc .class-description {',
+            '  font-style: italic;',
+            '}',
+            '',
+            '.jsdoc #main {',
+            '  float: left;',
+            '}',
+            '',
+            '.jsdoc section {',
+            '  display: block;',
+            '}',
+            '',
+            '.jsdoc .variation {',
+            '  display: none;',
+            '}',
+            '',
+            '.jsdoc .optional:after {',
+            '  font-style: italic;',
+            '  font-weight: lighter;',
+            '}',
+            '',
+            '.jsdoc nav {',
+            '  display: block;',
+            '}',
+            '',
+            '.jsdoc nav ul {',
+            '  line-height: 17px;',
+            '}',
+            '',
+            '.jsdoc nav h2 a,',
+            '.jsdoc nav h2 a:visited {',
+            '  text-decoration: none;',
+            '}',
+            '',
+            '.jsdoc nav h3 {',
+            '  margin-top: 12px;',
             '}' ]
       )
       t.end()
